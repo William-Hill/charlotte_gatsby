d258ba23f5b89c4b2431a49754f6d29a966ccda8
William Hill <mjh2225@gmail.com>
2019-07-28 18:14:10 -0700

    cleanup

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,34 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Content, { HTMLContent } from "../components/Content";
 
-export const AboutPageTemplate = ({ title, content, contentComponent, image }) => {
-  const PageContent = contentComponent || Content
-
-  console.log("image:", image)
+export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+  const PageContent = contentComponent || Content;
 
   return (
     <section className="hero is-fullheight">
-      <div className="hero-head">
-      </div>
+      <div className="hero-head"></div>
       <div className="hero-body">
         <PageContent className="content is-pulled-right" content={content} />
       </div>
       <div className="hero-foot"></div>
     </section>
-  )
-}
+  );
+};
 
 AboutPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func
-}
+};
 
 const AboutPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <Layout>
@@ -36,17 +33,16 @@ const AboutPage = ({ data }) => {
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
-        image={post.frontmatter.image}
       />
     </Layout>
-  )
-}
+  );
+};
 
 AboutPage.propTypes = {
-  data: PropTypes.object.isRequired,
-}
+  data: PropTypes.object.isRequired
+};
 
-export default AboutPage
+export default AboutPage;
 
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {
@@ -57,4 +53,4 @@ export const aboutPageQuery = graphql`
       }
     }
   }
-`
+`;
